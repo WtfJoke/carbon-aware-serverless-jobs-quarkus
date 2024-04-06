edf53a679d68d4ff1bb8fdf84e98d839594803d3
Manuel <wtfjoke@livenet.ch>
2024-04-06 21:09:29 +0200

    ✨ Add the possibility to pass state machine log options

--- infrastructure/src/main/java/io/github/wtfjoke/carbonawarecomputing/CarbonAwareComputingServerlessJobsConstructProps.java
+++ infrastructure/src/main/java/io/github/wtfjoke/carbonawarecomputing/CarbonAwareComputingServerlessJobsConstructProps.java
@@ -2,14 +2,28 @@ package io.github.wtfjoke.carbonawarecomputing;
 
 import software.amazon.awscdk.services.ssm.IStringParameter;
 import software.amazon.awscdk.services.stepfunctions.IChainable;
+import software.amazon.awscdk.services.stepfunctions.LogOptions;
 
 public record CarbonAwareComputingServerlessJobsConstructProps(IStringParameter apiKey,
 															   IChainable batchJobTask,
+															   LogOptions logOptions,
 															   Boolean useNativeImage
 ) {
 	public CarbonAwareComputingServerlessJobsConstructProps(IStringParameter apiKey,
 															IChainable batchJobTask) {
-		this(apiKey, batchJobTask, false);
+		this(apiKey, batchJobTask, null, false);
+	}
+
+	public CarbonAwareComputingServerlessJobsConstructProps(IStringParameter apiKey,
+															IChainable batchJobTask,
+															LogOptions logOptions) {
+		this(apiKey, batchJobTask, logOptions, false);
+	}
+
+	public CarbonAwareComputingServerlessJobsConstructProps(IStringParameter apiKey,
+															IChainable batchJobTask,
+															Boolean useNativeImage) {
+		this(apiKey, batchJobTask, null, useNativeImage);
 	}
 }
 
